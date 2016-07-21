d466040c8636f9a5f388c2bd0b38e2e82f5231ea
alxsmith <aedwards.smith@gmail.com>
2016-07-21 15:13:08 -0400

    first look at length dist
    SA and catch surveys

--- Montserrat/Alex_length/length_hists.R
+++ Montserrat/Alex_length/length_hists.R
@@ -22,43 +22,56 @@ species_CN <- unique(lapply(data$Species.common.name, tolower))
 
 species <- table(data$Species.ID)
 
+# Create sample size labels for plot
+n_df = data.frame(Species.ID=rownames(species[species > 30]), n=species[species > 30])
+n_df$xpos = c(24, 21, 38, 26, 30, 30, 40, 36)
+n_df$ypos = c(15, 20, 7.5, 10, 4, 6, 5, 20)
+
 data_threshold = data[data$Species.ID %in% names(species[species > 30]), ]
   
-
+#####
 #SA individual lengths data (generated with script 'extract_lenghts_SA.R')
-
+####
 SA_lengths <- read.csv('SA_individ_lengths.csv')
 
+SA_species <- table(SA_lengths$Species.ID)
+SA_n_df = data.frame(Species.ID=rownames(SA_species), n = SA_species)
+SA_n_df$xpos = c(0, 0, 0, 5, 25, 15, 10)
+SA_n_df$ypos = c(15, 150, 3, 10, 20, 3, 6)
 
 # Plot hists for length data
 library(ggplot2)
 
-length_h1 = ggplot(data = data_threshold, aes(x = Length..cm.)) +
-              geom_histogram(binwidth = 1) +
+length_h1 = ggplot(data = data_threshold) +
+              geom_histogram(aes(x = Length..cm.),binwidth = 1) +
+              geom_text(data=n_df,aes(x=xpos,y=ypos, label=paste("n=", n)), hjust=0) +
               theme_minimal()
 
-length_h2 = length_h1 + facet_wrap(~Species.ID, scales = "free") +
-  geom_text(aes(x=Inf,y=Inf, label=paste("n =", length(data_threshold$Length..cm.)))) 
-    
-    
+length_h2 = length_h1 + facet_wrap(~Species.ID, scales = "free") 
+ 
 
 
 # Plot hists for SA data
 library(ggplot2)
 
-SA_h1 = ggplot(data = SA_lengths, aes(x = length)) +
-  geom_histogram(binwidth = 1) +
+SA_h1 = ggplot(data = SA_lengths) +
+  geom_histogram(aes(x = length),binwidth = 1) +
+  geom_text(data=SA_n_df,aes(x=xpos,y=ypos, label=paste("n=", n.Freq)), hjust=0) +
   theme_minimal()
 
 SA_h2 = SA_h1 + facet_wrap(~Species.ID, scales = "free") 
-  #geom_text(aes(x=Inf,y=Inf, label=paste("n =", length(data_threshold$Length..cm.)))) 
+  
 
 
 
-    
-    #data=n_df, aes(x=30,y=25, label=paste("n =", n), hjust=-.1))
+
+geom_text(aes(x=Inf,y=Inf, label=paste("n =", length(data_threshold$Length..cm.)))) 
+
+
+
+
               
 #aes(x=Inf,y=Inf, label=paste("n =", length(data_threshold$Length..cm.)))) +
 
 
-#n_df = data.frame(n=species[species > 30])
+
