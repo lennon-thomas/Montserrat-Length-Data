06ad7756750fcd9aa7a2d077d8e65387f9c4fbef
alxsmith <aedwards.smith@gmail.com>
2016-08-10 12:21:23 -0400

    began explore LHI values
    trouble with
    LUTJMA M/K
    and Lm SCARVI

--- life history_AS.R
+++ life history_AS.R
@@ -1,16 +1,19 @@
 library(ggplot2)
 library(dplyr)
 
-lifehistory<-read.csv("Montserrat Life History Parms1.csv")
+lifehistory<-read.csv("Montserrat Life History Parms2.csv")
 lifehistory[is.na(lifehistory)] <- 0
 View(lifehistory)
 
 lifehistory$Value[lifehistory$Species=="ACANCO"&lifehistory$Parameter=="Linf"]
 
-#lifehistory<-na.omit(lifehistory)
-#lh2<-dcast(lifehistory,Species~X,value.var="Value",fun.aggreate=NULL)
+lifehistory<-na.omit(lifehistory)
+lh2<-dcast(lifehistory,Species~Parameter,value.var="Value",fun.aggreate=NULL)
 
 allspecies<-unique(lifehistory$Species)
+
+Lm <- data.frame(Species = allspecies, 
+                 Lm = rep(0, length(allspecies)))
 
 pdf('TESTlength.pdf')
 i=1
@@ -26,40 +29,52 @@ for (i in 1:length(allspecies))
   plotdf<-data.frame(age,length)  
   print(ggplot(plotdf,aes(x=age,y=length))+
     geom_line())
-  
+
+  Lm[i,2] <-  tail(length, n=1)
   print(i)
   
 }
 
 dev.off() # turns off pdf call
+# 
+# x<-(1:tmax[])
+# 
+# 
+# acanch<-lh2$Linf[1]*(1-exp(-lh2$K[1]*(x[]-lh2$t0[1])))
+# plot(acanch~x)
+# 
+# 
+# ?dcast
+# names(lifehistory)
+# View(lh2)
+# lvalues<-lifehistory$X=="Linf"
+# head(lh2)
+# View(lh2)
+# library(dplyr)
+# library(plyr)
+# library(tidyr)
+# library(reshape2)
+# library(reshape)
+# allspecies<-unique(lifehistory$Species)
+# a=1
+# 
+# 
+# for (a in 1:length(allspecies))
+#  {
+#   lifehistory %>%
+#     filter(Species==allspecies[a]) %>%
+#              ggplot(aes(x=Species, y=length)) +
+#              geom_bar(width=0.5)
+#  }
+#      dev.off()
+#      
 
-x<-(1:tmax[])
+# Test LHI 
+#
+lh2$Lm= Lm$Lm 
+
+LHI <- lh2 %>%
+  mutate(mk = M/K , LmLoo = Lm/Linf, Mtm = M * tmax) 
 
 
-acanch<-lh2$Linf[1]*(1-exp(-lh2$K[1]*(x[]-lh2$t0[1])))
-plot(acanch~x)
 
-
-?dcast
-names(lifehistory)
-View(lh2)
-lvalues<-lifehistory$X=="Linf"
-head(lh2)
-View(lh2)
-library(dplyr)
-library(plyr)
-library(tidyr)
-library(reshape2)
-library(reshape)
-allspecies<-unique(lifehistory$Species)
-a=1
-
-
-for (a in 1:length(allspecies)
-{
-  lifehistory %>%
-    filter(Species==allspecies[a] %>%
-             ggplot(aes(x=species, y=length)) +
-             geom_bar(width=0.5)
-}
-     dev.off()
