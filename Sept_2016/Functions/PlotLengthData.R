2a58f0ba971e3bfc2360917bd8555061754a1b99
lennon-thomas <lennon.rose.thomas@gmail.comgit config --global user.email lennon.rose.thomas@gmail.comgit config --global user.email lennon.rose.thomas@gmail.com>
2016-09-15 16:57:03 -0700

    length plots

--- Sept_2016/Functions/PlotLengthData.R
+++ Sept_2016/Functions/PlotLengthData.R
@@ -8,19 +8,19 @@ PlotLengthData<- function(LengthDat,FigureFolder,Fish,Species,Theme)
 
   #engthDat$SiteType[LengthDat$MPA==1]<- 'MPA'
   minimum<-min(LengthDat$Length)-5
-  maximum<-Fish$Linf+1
-  pdf(file=paste(FigureFolder,Species,'_Length Hist.pdf',sep=''),width=8,height=5)
-  
-  LengthDist<- ggplot(data=LengthDat,aes(Length,fill=(Data.Type)))+geom_density(alpha=0.6,aes(y=..count..))+
-  scale_fill_manual(name='Data Type',values=c(Fisherycolor,Surveycolor))+
+  maximum<-Fish$Linf+10
+ pdf(file=paste(FigureFolder,Species[i],'_Length Hist.pdf',sep=''),width=7,height=5)
+  LengthDist<-ggplot(data=LengthDat,aes(Length,fill=(Data.Type)))+geom_density(alpha=0.6,aes(y=..count..))+
   geom_vline(xintercept=Fish$Mat50,linetype='longdash')+
     geom_vline(xintercept=Fish$Linf,linetype='longdash',color='red2')+
-   # facet_wrap(paste("n =",length(LengthData$Length),sep=""),as.table=F)+
+    #facet_wrap(paste("n =",length(LengthData$Length),sep=""),as.table=F)+
     #facet_wrap(~Year,as.table=F)+
    ggtitle(paste(Species,sep='\nn = ',length(LengthDat$Length)))+Theme+xlab("Length (cm)")+ylab("Frequency")+
-    scale_x_continuous(limits=c(minimum,maximum),expand = c(0, 0)) +
+    scale_x_continuous(limits=c(minimum,maximum),expand = c(0, 0))+ 
     scale_y_continuous(expand = c(0, 0))
+    LengthDist<-LengthDist+labs(fill="Data Type")
     print(LengthDist)
-  dev.off()
+    dev.off()
+ 
 }
 
